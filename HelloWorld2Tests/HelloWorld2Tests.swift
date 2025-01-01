d73b644f0e804169318d81517edf64e5c9af16bc
Oksana <grigorivoksana@gmail.com>
2025-01-01 20:07:10 +0100

    Initial Commit

---
+++ HelloWorld2Tests/HelloWorld2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld2Tests.swift
+//  HelloWorld2Tests
+//
+//  Created by Oksana on 01.01.2025.
+//
+
+import Testing
+@testable import HelloWorld2
+
+struct HelloWorld2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
